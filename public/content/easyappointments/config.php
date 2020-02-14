9f3556fc172bed0119932be4347624b8603ec723
Shubham Arawkar <arawkar.shubham08@gmail.com>
2020-02-14 23:01:55 +0530

    [FIX] converted const to var

--- public/content/easyappointments/config.php
+++ public/content/easyappointments/config.php
@@ -37,10 +37,10 @@ class Config {
     // ------------------------------------------------------------------------
     // DATABASE SETTINGS
     // ------------------------------------------------------------------------
-    const DB_HOST       = trim(parse_url(getenv("CLEARDB_DATABASE_URL"))["host"]);
-    const DB_NAME       = trim(parse_url(getenv("CLEARDB_DATABASE_URL"))["path"], "/");
-    const DB_USERNAME   = trim(parse_url(getenv("CLEARDB_DATABASE_URL"))["user"]);
-    const DB_PASSWORD   = trim(parse_url(getenv("CLEARDB_DATABASE_URL"))["pass"]);
+    var DB_HOST       = trim(parse_url(getenv("CLEARDB_DATABASE_URL"))["host"]);
+    var DB_NAME       = trim(parse_url(getenv("CLEARDB_DATABASE_URL"))["path"], "/");
+    var DB_USERNAME   = trim(parse_url(getenv("CLEARDB_DATABASE_URL"))["user"]);
+    var DB_PASSWORD   = trim(parse_url(getenv("CLEARDB_DATABASE_URL"))["pass"]);
 
     // ------------------------------------------------------------------------
     // GOOGLE CALENDAR SYNC

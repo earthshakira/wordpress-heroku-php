29581c29fba062176d42ee2dfa8b1797cfab1a05
Shubham Arawkar <arawkar.shubham08@gmail.com>
2020-02-14 23:13:17 +0530

    [FIX] created a writer file

---
+++ public/content/easyappointments/create_config.php
@@ -0,0 +1,39 @@
+ <?php
+
+$file = fopen("config.php","w");
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$name = trim($url["path"], "/");
+$user = trim($url["user"]);
+$pass = trim($url["pass"]);
+$host = trim($url["host"]);
+
+echo fwrite($file,"class Config {
+    // ------------------------------------------------------------------------
+    // GENERAL SETTINGS
+    // ------------------------------------------------------------------------
+
+    const BASE_URL      = 'http://easy-appoints.herokuapp.com/content/easyappointments';
+    const LANGUAGE      = 'english';
+    const DEBUG_MODE    = TRUE;
+
+    // ------------------------------------------------------------------------
+    // DATABASE SETTINGS
+    // ------------------------------------------------------------------------
+
+    const DB_HOST       = '".$host."';
+    const DB_NAME       = '".$name."';
+    const DB_USERNAME   = '".$user."';
+    const DB_PASSWORD   = '".$pass."';
+
+    // ------------------------------------------------------------------------
+    // GOOGLE CALENDAR SYNC
+    // ------------------------------------------------------------------------
+
+    const GOOGLE_SYNC_FEATURE   = FALSE; // Enter TRUE or FALSE
+    const GOOGLE_PRODUCT_NAME   = '';
+    const GOOGLE_CLIENT_ID      = '';
+    const GOOGLE_CLIENT_SECRET  = '';
+    const GOOGLE_API_KEY        = '';
+}");
+fclose($file);
+?> 
